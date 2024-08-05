59309fa6b4f9842421cebab973acdd27d9360683
Douglas Everbero <dougas@3xweb.site>
2024-08-05 15:17:33 -0300

    chore: adiciona o nome do método de pagamento ao invez de other

--- includes/payments/class-wc-guru-payment-other.php
+++ includes/payments/class-wc-guru-payment-other.php
@@ -1,7 +1,8 @@
 <?php
 class WC_Guru_Payment_Other extends WC_Guru_Payment_Base {
+       
     public function process_order($order, $new_status) {
-        $this->get_order_items($order, 'other');
+        $this->get_order_items($order, $order->get_payment_method_title( "view" ));
         $this->update_order_status($order, $new_status);
     }
 }
